2e2e9f5e4164bdcb46a9aeb6037172553df56ef4
shojib deb <shodeb123@gmail.com>
2023-01-23 15:47:47 +0000

    GET: Flashcards category by button ID

--- client/assets/index.js
+++ client/assets/index.js
@@ -1,17 +1,19 @@
 
+
 var next = 0;
 var fLength = 0;
-async function displayFlashcard(next) {
-  const res = await fetch("http://localhost:3000/flashcard");
+
+
+async function displayFlashcard(next, category) {
+  const res = await fetch(`http://localhost:3000/flashcard/${category}`);
   const flashcard = await res.json();
   fLength = flashcard.length
+  console.log(fLength)
   const contentElement = document.querySelector("#content");
   const answer1Element = document.querySelector("#answer1");
   const answer2Element = document.querySelector("#answer2");
   const answer3Element = document.querySelector("#answer3");
   const answer4Element = document.querySelector("#answer4");
-
-
 
   contentElement.textContent = flashcard[next]["content"];
   answer1Element.textContent = flashcard[next]["answer1"];
@@ -20,16 +22,31 @@ async function displayFlashcard(next) {
   answer4Element.textContent = flashcard[next]["answer4"];
 }
 
-const show = document.querySelector("#show-button");
-show.addEventListener('click', displayFlashcard(next));
 
-document.getElementById("next-button").onclick = function () {
-  if (next++ < fLength) {
-    displayFlashcard(next++)
+
+
+ const show = document.querySelector(".sub-button");
+
+ let sub = show.addEventListener('click', (e) => {
+    sub = e.target.id
+ })
+ 
+ show.addEventListener('click', function () {displayFlashcard(next, sub)});
+
+
+
+
+
+document.getElementById("next-button").onclick =  function () {
+  
+  if (next < fLength - 1) {
+    displayFlashcard((next = next + 1), sub)
   }
   else {
     console.log("there are no more flashcards left!!!")
   }
+
+  
 };
 
 
