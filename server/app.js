474d2afee03f569e416377460a54d3c36df1fa64
shojib deb <shodeb123@gmail.com>
2023-01-23 10:53:25 +0000

    GET: collection of current flashcards

--- server/app.js
+++ server/app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const cors = require('cors');
 const logger = require("./logger");
+const flashcard = require('./flashcard');
 const app = express();
 
 app.use(cors());
 app.use(express.json())
 app.use(logger);
 
+app.get("/", (req, res) => {
+    res.send(`Welcome to the rampant-goats API! There are ${flashcard.length} available.`);
+})
 
 
+app.get('/flashcard', (req, res) => {
+    res.json(flashcard);
+})
 
 
 
