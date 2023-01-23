36d86dedf305141619b710f9539aa4c42044be3f
shojib deb <shodeb123@gmail.com>
2023-01-23 10:47:56 +0000

    Add flashcards json

--- server/app.js
+++ server/app.js
@@ -3,7 +3,14 @@ const cors = require('cors');
 const logger = require("./logger");
 const app = express();
 
-
 app.use(cors());
 app.use(express.json())
 app.use(logger);
+
+
+
+
+
+
+
+module.exports = app;
