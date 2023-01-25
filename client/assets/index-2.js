1aae75680658338001af91ccda570d49b702cdc1
dominika militowska <dominika.militowska@gmail.com>
2023-01-25 18:41:48 +0000

    css and some links done

--- client/assets/index-2.js
+++ client/assets/index-2.js
@@ -1,30 +1,30 @@
 
 // // //////// Hamburger menu on click event to pull out sidebar animation START /////////////
-// document.querySelector(".hamburger-menu").addEventListener("click", () => {
-//   document.querySelector(".container").classList.toggle("change")
-// })
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+  document.querySelector(".container").classList.toggle("change")
+})
 /////////// Hamburger menu on click event to pull out sidebar animation END /////////////
 
 
 
-//////// Switch color button functionality START ///////////////
-// const switchColorBtn = document.querySelector("#switch-color")
-// const body = document.querySelector("body")
-// const flashcard = document.querySelector(".flashcard")
+////// Switch color button functionality START ///////////////
+const switchColorBtn = document.querySelector("#switch-color")
+const body = document.querySelector("body")
+const flashcard = document.querySelector(".flashcard")
 
 
-// let bodyColors = ["#0081C9", "white"];
-// let flashcardColors = ["#FFC93C", "purple"];
-// let bodyColorIndex = 0;
-// let flashcardColorIndex = 0;
+let bodyColors = ["#0081C9", "white"];
+let flashcardColors = ["#FFC93C", "purple"];
+let bodyColorIndex = 0;
+let flashcardColorIndex = 0;
 
-// switchColorBtn.addEventListener("click", function () {
-//   body.style.backgroundColor = bodyColors[bodyColorIndex];
-//   flashcard.style.backgroundColor = flashcardColors[flashcardColorIndex];
-//   bodyColorIndex = (bodyColorIndex + 1) % bodyColors.length;
-//   flashcardColorIndex = (flashcardColorIndex + 1) % flashcardColors.length;
-// });
-//////// Switch color button functionality END /////////////
+switchColorBtn.addEventListener("click", function () {
+  body.style.backgroundColor = bodyColors[bodyColorIndex];
+  flashcard.style.backgroundColor = flashcardColors[flashcardColorIndex];
+  bodyColorIndex = (bodyColorIndex + 1) % bodyColors.length;
+  flashcardColorIndex = (flashcardColorIndex + 1) % flashcardColors.length;
+});
+////// Switch color button functionality END /////////////
 
 
 
