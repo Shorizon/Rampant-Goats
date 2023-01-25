4145b087cf11ae851944917e5f2d489f9c1d11b3
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-01-25 11:50:15 +0000

    Added some testing and moved images file to assets

--- client/assets/index-2.test.js
+++ client/assets/index-2.test.js
@@ -1,39 +1,57 @@
 
-describe("Hamburger menu functionality", () => {
-    it("should toggle the class 'change' on the container element when clicked", () => {
-      document.body.innerHTML = `
-        <div class="container"></div>
-        <div class="hamburger-menu"></div>
-      `;
-      const container = document.querySelector(".container");
-      const hamburgerMenu = document.querySelector(".hamburger-menu");
-      hamburgerMenu.addEventListener("click", () => {
-        container.classList.toggle("change")
-      });
-      hamburgerMenu.click();
-      expect(container.classList.contains("change")).toBe(true);
-      hamburgerMenu.click();
-      expect(container.classList.contains("change")).toBe(false);
+const { flipCard } = require("./index-2.js")
+const { displayFlashcard } = require("./index-2.js")
+const { updateProgress } = require("./index-2.js")
+const { previousCard } = require("./index-2.js")
+const { sendData } = require("./index-2.js")
+
+
+describe('flipCard', () => {
+    it('should toggle flip class on card element', () => {
+      const card = document.createElement('div');
+      flipCard();
+      expect(card.classList.contains('flip')).toBe(true);
+      flipCard();
+      expect(card.classList.contains('flip')).toBe(false);
     });
-  });
+  });  
+
+
+// describe("Hamburger menu functionality", () => {
+//     it("should toggle the class 'change' on the container element when clicked", () => {
+//       document.body.innerHTML = `
+//         <div class="container"></div>
+//         <div class="hamburger-menu"></div>
+//       `;
+//       const container = document.querySelector(".container");
+//       const hamburgerMenu = document.querySelector(".hamburger-menu");
+//       hamburgerMenu.addEventListener("click", () => {
+//         container.classList.toggle("change")
+//       });
+//       hamburgerMenu.click();
+//       expect(container.classList.contains("change")).toBe(true);
+//       hamburgerMenu.click();
+//       expect(container.classList.contains("change")).toBe(false);
+//     });
+//   });
   
-  describe("Switch color button functionality", () => {
-    it("should change the background color of the body and flashcard elements when clicked", () => {
-      document.body.innerHTML = `
-        <body></body>
-        <div class="flashcard"></div>
-        <button id="switch-color"></button>
-      `;
-      const switchColorBtn = document.querySelector("#switch-color");
-      const body = document.querySelector("body");
-      const flashcard = document.querySelector(".flashcard");
-      switchColorBtn.addEventListener("click", function () {
-        body.style.backgroundColor = "#0081C9";
-        flashcard.style.backgroundColor = "#FFC93C";
-      });
-      switchColorBtn.click();
-      expect(body.style.backgroundColor).toBe("#0081C9");
-      expect(flashcard.style.backgroundColor).toBe("#FFC93C");
-    });
-  });
+//   describe("Switch color button functionality", () => {
+//     it("should change the background color of the body and flashcard elements when clicked", () => {
+//       document.body.innerHTML = `
+//         <body></body>
+//         <div class="flashcard"></div>
+//         <button id="switch-color"></button>
+//       `;
+//       const switchColorBtn = document.querySelector("#switch-color");
+//       const body = document.querySelector("body");
+//       const flashcard = document.querySelector(".flashcard");
+//       switchColorBtn.addEventListener("click", function () {
+//         body.style.backgroundColor = "#0081C9";
+//         flashcard.style.backgroundColor = "#FFC93C";
+//       });
+//       switchColorBtn.click();
+//       expect(body.style.backgroundColor).toBe("#0081C9");
+//       expect(flashcard.style.backgroundColor).toBe("#FFC93C");
+//     });
+//   });
   
