c37199d5daa993592062dc36afc4dc3f9fe43f70
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-01-26 08:56:58 +0000

    Tried moving over function into separate file and testing

--- client/assets/test/index-2.test.js
+++ client/assets/test/index-2.test.js
@@ -8,17 +8,35 @@ const path = require('path');
 const html = fs.readFileSync(path.resolve(__dirname, '../../index-2.html'), 'utf8').toString();
 // const { document } = new JSDOM(html).window;
 document.documentElement.innerHTML = html
-const { flipCard } = require("../index-2.js")
+const { increaseCount, updateProgress } = require("./functions.js")
 
-test("There is a thing in the document", () => {
-    const button = document.querySelector("#clicker");
-    expect(button).not.toBe(null);
-})
+test('increaseCount function should return 1', () => {
+    expect(increaseCount()).toBe(1);
+  });
+  
+test('updateProgress function should update the width and text of the progressBarFill', () => {
+    // Create a mock element to represent the progressBarFill
+    const progressBarFill = { style: { width: '0%' }, textContent: '' };
+    // Call the updateProgress function with a questionNum of 2
+    updateProgress(2,progressBarFill);
+    // Assert that the width and text of the progressBarFill have been updated correctly
+    expect(progressBarFill.style.width).toBe('40%');
+    expect(progressBarFill.textContent).toBe('2/5');
+});
+  
 
-test("testing adding and removing flip class", () => {
-    const card = document.getElementById("flashcard")
-    const subButton = document.getElementById("submit-button");
-    subButton.addEventListener("click", flipCard);
-    subButton.click();
-    expect(card.classList.contains("flip")).toBe(false)
-})
+
+
+
+// test("There is a thing in the document", () => {
+//     const button = document.querySelector("#clicker");
+//     expect(button).not.toBe(null);
+// })
+
+// test("testing adding and removing flip class", () => {
+//     const card = document.getElementById("flashcard")
+//     const subButton = document.getElementById("submit-button");
+//     subButton.addEventListener("click", flipCard);
+//     subButton.click();
+//     expect(card.classList.contains("flip")).toBe(false)
+// })
