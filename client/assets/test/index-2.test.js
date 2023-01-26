e7d8d2ce26f300a64ef06c9b15d8f2330c46e202
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-01-26 08:44:39 +0000

    Tried to get the test suite working

--- client/assets/test/index-2.test.js
+++ client/assets/test/index-2.test.js
@@ -2,17 +2,23 @@
  * @jest-environment jsdom
  */
 
-const { JSDOM } = require("jsdom");
+// const { JSDOM } = require("jsdom");
 const fs = require('fs');
 const path = require('path');
-const html = fs.readFileSync(path.resolve(__dirname, '../../index-2.html'), 'utf8');
-const { document } = new JSDOM(html).window;
-const { flipCard } = require("./index-2.js")
+const html = fs.readFileSync(path.resolve(__dirname, '../../index-2.html'), 'utf8').toString();
+// const { document } = new JSDOM(html).window;
+document.documentElement.innerHTML = html
+const { flipCard } = require("../index-2.js")
+
+test("There is a thing in the document", () => {
+    const button = document.querySelector("#clicker");
+    expect(button).not.toBe(null);
+})
 
 test("testing adding and removing flip class", () => {
     const card = document.getElementById("flashcard")
     const subButton = document.getElementById("submit-button");
     subButton.addEventListener("click", flipCard);
     subButton.click();
-    expect(card.classList.contains("flip")).toBe(true)
+    expect(card.classList.contains("flip")).toBe(false)
 })
