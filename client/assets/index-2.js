764ad8ffd711a6464abf9acfcc46bc3c0e548530
shojib deb <shodeb123@gmail.com>
2023-01-26 09:23:50 +0000

    removed superflous <p>

--- client/assets/index-2.js
+++ client/assets/index-2.js
@@ -33,7 +33,6 @@
 //////// View flashcards in categories START /////////////
 let next = 0;
 let fLength, sub, counterQ, correct = undefined;
-let arr = [];
 let scoreboard = 0;
 
 const queryParams = new URLSearchParams(window.location.search);
@@ -98,19 +97,6 @@ function flipCard() {
     i++;
   })
 }
-//////// Submit button revels back of flashcard /////////////
-
-
-// const showButtons = Array.from(document.getElementsByClassName("sub-button"));
-
-// showButtons.forEach(e => {
-//   e.addEventListener('click', (f) => {
-//     sub = f.target.id
-//   })
-//   e.addEventListener('click', function () { displayFlashcard(next, sub) })
-// })
-//////// View flashcards in categories END /////////////
-
 
 //////// Go to the nextprevious flashcard START /////////////
 const nextButton = document.getElementById("next-button-front")
@@ -147,71 +133,6 @@ function previousCard() {
   }
 };
 ////// Go to the nextprevious flashcard END /////////////
-
-//////// Read from uploaded json file START /////////////
-
-document.getElementById('import').onclick = async function () {
-  let files = document.getElementById('selectFiles').files;
-  if (!files.length)
-    return false;
-  let scanner = new FileReader();
-
-  scanner.onload = function (e) {
-    let result = JSON.parse(e.target.result);
-    for (let key in result) {
-      for (let key2 in key) {
-        data = result[key][key2]
-        if (data != undefined)
-          arr.push(data)
-      }
-    }
-    sendData(arr);
-  }
-
-  scanner.readAsText(files.item(0));
-
-};
-//////// Read from uploaded json file End /////////////
-
-////////// Send the fetch request over to the server START /////////////
-async function sendData(arr) {
-
-  console.log(arr.length + "check")
-
-
-  for (let e of arr) {
-    let data = {
-      'content': e.content,
-      'answer1': e.answer1,
-      'answer2': e.answer2,
-      'answer3': e.answer3,
-      'answer4': e.answer4,
-      'corAnswer': e.corAnswer,
-      'category': e.category,
-      'corIndex': e.corIndex
-    }
-
-
-    const options = {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    }
-
-    const response = await fetch("http://localhost:3000/flashcard", options)
-
-    if (response.status == 201) {
-      console.log("received")
-    }
-  }
-}
-////////// Send the fetch request over to the server END /////////////
-
-
-
 
 
 module.exports = {
