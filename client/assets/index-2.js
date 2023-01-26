709a98ecb1ce22c9c3c89c328cbc4724e939c768
Mark Halstead <103516032+Mark-Halstead@users.noreply.github.com>
2023-01-26 14:03:01 +0000

    Added new landing page

--- client/assets/index-2.js
+++ client/assets/index-2.js
@@ -71,7 +71,6 @@ const progressBarFill = document.querySelector('.progress-bar-fill');
 function updateProgress(questionNum) {
   progressBarFill.style.width = (questionNum * (0.2 * 100)) + '%';
   progressBarFill.textContent = ((`${counterQ}/${fLength}`))
-
 }
 /////////// Progress bar live update END /////////////
 
