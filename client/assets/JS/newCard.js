b0c9634c06963cdb1a76fd39df59c5dec952b26e
shojib deb <shodeb123@gmail.com>
2023-01-27 12:12:55 +0000

    fix flashcards

--- client/assets/JS/newCard.js
+++ client/assets/JS/newCard.js
@@ -1,4 +1,4 @@
-let question = document.getElementById("question");
+let question = document.getElementById("content");
 let answer1 = document.getElementById("ans1");
 let answer2 = document.getElementById("ans2");
 let answer3 = document.getElementById("ans3");
@@ -7,6 +7,7 @@ let corAnswer = document.getElementById("cor");
 let corIndex = document.getElementById("indCor");
 let category = document.getElementById("cat");
 let data = {};
+let arrCat = ["History","Art","Music","English"]
 
 
 document.getElementById("create").addEventListener('click', newCard)
@@ -15,6 +16,30 @@ document.getElementById("create").addEventListener('click', newCard)
 async function newCard() {
 
     if (question.value, answer1.value, answer2.value, answer3.value, answer4.value, corAnswer.value, corIndex.value, category.value != undefined) {
+        
+        if (isNaN(corIndex.value) != false || parseInt(corIndex.value) > 3 || parseInt(corIndex.value) < 0){
+            alert("the correct index should be between 0 and 3")
+            corIndex.value = ""
+            return
+        
+        }
+
+        if(!arrCat.includes(capitalize(category.value))){
+            alert("please choose a category between: History,Art,Music and English")
+            category.value= ""
+            return
+        }
+
+        if(corAnswer.value == answer1.value || corAnswer.value == answer3.value || corAnswer.value == answer3.value){
+            
+        }else{
+            alert("please enter a correct answer that is the same as one of the given answers")
+            corAnswer.value= ""
+            return
+        }
+
+
+
         data = {
             'content': question.value,
             'answer1': answer1.value,
@@ -23,9 +48,10 @@ async function newCard() {
             'answer4': answer4.value,
             'corAnswer': corAnswer.value,
             'category': category.value,
-            'corIndex': corIndex.value
+            'corIndex': parseInt(corIndex.value)
         }
 
+        console.log(data)
         const options = {
             method: "POST",
             body: JSON.stringify(data),
@@ -50,4 +76,9 @@ async function newCard() {
 
 
 
+}
+
+function capitalize(s)
+{
+    return s[0].toUpperCase() + s.slice(1);
 }
